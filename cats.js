c1c1d40e8e49dac3bf25f7020495c6b0983899f5
Zexing Li <LiZexing@Zers-Mac.local>
2018-09-15 21:12:29 +0800

    lab1

---
+++ cats.js
@@ -0,0 +1,11 @@
+//test about lab 1... follow Chris's instructions
+let cats = [
+    {name: 'july', likes:'sleep'},
+    {name: 'sweet', likes:'eat'},
+    {name: 'wooo', likes:'woooo'},
+
+]
+for (let i=0;i<cats.length; i++) {
+    console.log(` ${cats[i].name} likes   ${cats[i].likes}`);
+
+}
