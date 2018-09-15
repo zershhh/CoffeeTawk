31ba8b0d2c9f9224b3b03700a61e006687b970d7
Zexing Li <LiZexing@Zers-Mac.local>
2018-09-15 19:27:40 +0800

    challenge 1

---
+++ coffeetawk.js
@@ -0,0 +1,11 @@
+//CoffeeTawk — by Zexing Li — applab, lab 1, 2018
+Challenge 1:
+1. Larger demand  in Shanghai than New York
+2. 1 more minite to take order and 5 minite to get the drink
+3. Most people use Alipay/Wechat, so payment is quick
+4. Most people know what to order before entering
+5. Customers are unhappy about the queue.
+6. Baristas are handling both ordering and making coffee. 
+
+Challenge 2 :
+
